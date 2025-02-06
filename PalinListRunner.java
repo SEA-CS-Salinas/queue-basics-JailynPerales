94129a324235fd8e750d98b1dec098a978508ade
JailynPerales <132614537+JailynPerales@users.noreply.github.com>
2025-02-06 10:28:57 -0600

    Update PalinListRunner.java

--- PalinListRunner.java
+++ PalinListRunner.java
@@ -1,12 +1,15 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
+//Name - Jailyn Perales
 
 public class PalinListRunner
 {
 	public static void main ( String[] args )
 	{
-		//add test cases			
+		//add test cases
+		PalinList s = new PalinList("one two one");
+		System.out.println(s);
 	}
 }
+
